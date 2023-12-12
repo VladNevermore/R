c73b3cb5f1387ee1c99484cd0852c3e87d2b64cd
VladNevermore <151689422+VladNevermore@users.noreply.github.com>
2023-12-12 22:43:35 +0500

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,69 +1,73 @@
 console.log('sound');
 
 $(document).ready(function() {
-  $('.playpause').click(function () {
-    if (Tone.context.state !== 'running') {
-      Tone.context.resume().then(() => {
-        console.log('Audio context resumed successfully');
-      }).catch((err) => {
-        console.error('Error when trying to resume audio context:', err);
-      });
-    }
-  });
-
-  var buffer = new Tone.Buffer("WTFW.mp3", function(){
-    console.log('buffer')
-
-    var meter = new Tone.Meter();
-
-    var player = new Tone.Player({
-      "url" : buffer.get()
-    }).connect(meter).toMaster();
-
-    const button = $('#button');
-    const enter = $('#enter');
-
-    enter.fadeIn();
-
-    enter.click(function() {
-      $('body').removeClass('scroll-lock');
-      $('.container').fadeIn();
-      player.start();
-      enter.fadeOut();
-      button.attr('checked', true);
+  // Проверяем и восстанавливаем контекст аудио
+  if (Tone.context.state !== 'running') {
+    Tone.context.resume().then(() => {
+      console.log('Audio context resumed successfully');
+      playMusic(); // Запускаем воспроизведение музыки
+    }).catch((err) => {
+      console.error('Error when trying to resume audio context:', err);
     });
+  } else {
+    playMusic(); // Запускаем воспроизведение музыки
+  }
 
-    button.change(function(){
-      if(player.state === 'started') {
-        if(player.mute) {
-          player.mute = false;
-          button.attr('checked', false);
-        } else {
-          player.mute = true;
-          $(this).attr('checked', true);
-        }
-      } else if (player.state === 'stopped') {
+  function playMusic() {
+    var buffer = new Tone.Buffer("WTFW.mp3", function(){
+      console.log('buffer')
+
+      var meter = new Tone.Meter();
+
+      var player = new Tone.Player({
+        "url" : buffer.get()
+      }).connect(meter).toMaster();
+
+      const button = $('#button');
+      const enter = $('#enter');
+
+      enter.fadeIn();
+
+      enter.click(function() {
+        $('body').removeClass('scroll-lock');
+        $('.container').fadeIn();
         player.start();
         enter.fadeOut();
         button.attr('checked', true);
+      });
+
+      button.change(function(){
+        if(player.state === 'started') {
+          if(player.mute) {
+            player.mute = false;
+            button.attr('checked', false);
+          } else {
+            player.mute = true;
+            $(this).attr('checked', true);
+          }
+        } else if (player.state === 'stopped') {
+          player.start();
+          enter.fadeOut();
+          button.attr('checked', true);
+        }
+      });
+
+      function opacityLevel(){
+        var level = meter.getLevel();
+        level = Tone.dbToGain(level); //scale it between 0 - 1
+        opacity = 1-(Math.round(level * 175) / 100);
+        if (opacity < 0.25) {
+          $('#main').css('opacity', 0.25);
+        } else {
+          $('#main').css('opacity', opacity);
+        }
       }
+
+      function loop(){
+        requestAnimationFrame(loop);
+        opacityLevel();
+      }
+      loop();
     });
-
-    function opacityLevel(){
-      var level = meter.getLevel();
-      level = Tone.dbToGain(level); //scale it between 0 - 1
-      opacity = 1-(Math.round(level * 175) / 100);
-      if (opacity < 0.25) {
-        $('#main').css('opacity', 0.25);
-      } else {
-        $('#main').css('opacity', opacity);
-      }
-    }
-
-    function loop(){
-      requestAnimationFrame(loop);
-      opacityLevel();
-    }
-    loop();
-  });
+  }
 });
