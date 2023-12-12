53e97e2bc77a065bdc70e7ae311d0704da8dfe40
VladNevermore <151689422+VladNevermore@users.noreply.github.com>
2023-12-12 22:34:53 +0500

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,64 +1,69 @@
 console.log('sound');
 
-$('.playpause').click(function () {
-  if (Tone.context.state !== 'running') {
-    Tone.context.resume();
-  }
-})
-
-var buffer = new Tone.Buffer("WTFW.mp3", function(){
-  
-  console.log('buffer')
-  
-  var meter = new Tone.Meter();
-
-  var player = new Tone.Player({
-    "url" : buffer.get()
-  }).connect(meter).toMaster();
-
-  const button = $('#button');
-  const enter = $('#enter');
-
-  enter.fadeIn();
-
-  enter.click(function() {
-    $('body').removeClass('scroll-lock');
-    $('.container').fadeIn();
-    player.start();
-    enter.fadeOut();
-    button.attr('checked', true);
-  })
-
-  button.change(function(){
-    if(player.state === 'started') {
-      if(player.mute) {
-        player.mute = false;
-        button.attr('checked', false);
-      } else {
-        player.mute = true;
-        $(this).attr('checked', true);
-      }
-    } else if (player.state === 'stopped') {
-      player.start();
-      enter.fadeOut();
-      button.attr('checked', true);
+$(document).ready(function() {
+  $('.playpause').click(function () {
+    if (Tone.context.state !== 'running') {
+      Tone.context.resume().then(() => {
+        console.log('Audio context resumed successfully');
+      }).catch((err) => {
+        console.error('Error when trying to resume audio context:', err);
+      });
     }
   });
 
-  function opacityLevel(){
-    var level = meter.getLevel();
-    level = Tone.dbToGain(level); //scale it between 0 - 1
-    opacity = 1-(Math.round(level * 175) / 100);
-    if (opacity < 0.25) {
-      $('#main').css('opacity', 0.25);
-    } else {
-      $('#main').css('opacity', opacity);
-    }
-  }
+  var buffer = new Tone.Buffer("WTFW.mp3", function(){
+    console.log('buffer')
 
-  function loop(){
-    requestAnimationFrame(loop);
-    opacityLevel();
-  }
-  loop();
+    var meter = new Tone.Meter();
+
+    var player = new Tone.Player({
+      "url" : buffer.get()
+    }).connect(meter).toMaster();
+
+    const button = $('#button');
+    const enter = $('#enter');
+
+    enter.fadeIn();
+
+    enter.click(function() {
+      $('body').removeClass('scroll-lock');
+      $('.container').fadeIn();
+      player.start();
+      enter.fadeOut();
+      button.attr('checked', true);
+    });
+
+    button.change(function(){
+      if(player.state === 'started') {
+        if(player.mute) {
+          player.mute = false;
+          button.attr('checked', false);
+        } else {
+          player.mute = true;
+          $(this).attr('checked', true);
+        }
+      } else if (player.state === 'stopped') {
+        player.start();
+        enter.fadeOut();
+        button.attr('checked', true);
+      }
+    });
+
+    function opacityLevel(){
+      var level = meter.getLevel();
+      level = Tone.dbToGain(level); //scale it between 0 - 1
+      opacity = 1-(Math.round(level * 175) / 100);
+      if (opacity < 0.25) {
+        $('#main').css('opacity', 0.25);
+      } else {
+        $('#main').css('opacity', opacity);
+      }
+    }
+
+    function loop(){
+      requestAnimationFrame(loop);
+      opacityLevel();
+    }
+    loop();
+  });
 });
