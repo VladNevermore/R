240afe926f720e287dbb9dc38a868edce68c0a6f
VladNevermore <151689422+VladNevermore@users.noreply.github.com>
2023-12-14 09:37:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,6 @@
 $(document).ready(function () {
     $('img').attr('draggable', false);
 
-    let initialTouches = [];
-    let currentScale = 1;
-    let currentTranslate = { x: 0, y: 0 };
-
-    const imageContainer = $('.image-container');
-    const modal = $("#myModal");
-    const modalImg = $("#modalImg");
-
-    imageContainer.on('touchstart', function (e) {
-        initialTouches = e.originalEvent.touches;
-    });
-
-    imageContainer.on('touchmove', function (e) {
-        const currentTouches = e.originalEvent.touches;
-
-        if (initialTouches.length === 2 && currentTouches.length === 2) {
-            const deltaX = currentTouches[1].clientX - currentTouches[0].clientX;
-            const deltaY = currentTouches[1].clientY - currentTouches[0].clientY;
-            const initialDistance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
-
-            const newDeltaX = initialTouches[1].clientX - initialTouches[0].clientX;
-            const newDeltaY = initialTouches[1].clientY - initialTouches[0].clientY;
-            const newDistance = Math.sqrt(newDeltaX ** 2 + newDeltaY ** 2);
-
-            const scaleChange = newDistance / initialDistance;
-            currentScale *= scaleChange;
-
-            imageContainer.find('img').css({
-                'transform': `scale(${currentScale}) translate(${currentTranslate.x}px, ${currentTranslate.y}px)`
-            });
-        }
-
-        initialTouches = currentTouches;
-    });
-
-    imageContainer.on('touchend', function () {
-        initialTouches = [];
-    });
-
     $('body')[0].onmousemove = (function () {
         var onmousestop = function () {
             $('.container').addClass('transparent');
@@ -67,21 +28,6 @@ $(document).ready(function () {
             gifContainer.css('opacity', 0);
             audio.pause();
             audio.currentTime = 0;
-        }
-    });
-
-    imageContainer.find('img').on('click', function () {
-        modal.css('display', 'block');
-        modalImg.attr('src', $(this).attr('src'));
-    });
-
-    $('#myModal .close').on('click', function () {
-        modal.css('display', 'none');
-    });
-
-    $(window).on('click', function (event) {
-        if (event.target === modal[0]) {
-            modal.css('display', 'none');
         }
     });
 });
