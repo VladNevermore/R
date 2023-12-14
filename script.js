5683fb122fdfbe5507dd7578592be31a5d2a4a70
VladNevermore <151689422+VladNevermore@users.noreply.github.com>
2023-12-14 07:52:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-$('img').attr('draggable', false);
+$(document).ready(function() {
+  $('img').attr('draggable', false);
 
-$('body')[0].onmousemove = (function() {
+  $('body')[0].onmousemove = (function() {
     var onmousestop = function() {
-        $('.container').addClass('transparent');
+      $('.container').addClass('transparent');
     }, thread;
 
     return function() {
-        $('.container').removeClass('transparent');
-        clearTimeout(thread);
-        thread = setTimeout(onmousestop, 3000);
+      $('.container').removeClass('transparent');
+      clearTimeout(thread);
+      thread = setTimeout(onmousestop, 3000);
     };
-})();
+  })();
 
-var gifContainer = document.getElementById('gif-container');
-var audio = document.getElementById('audio');
+  var gifContainer = document.getElementById('gif-container');
+  var audio = document.getElementById('audio');
 
-window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function() {
     var endSection = document.getElementById('endSection');
     var endSectionBottom = endSection.getBoundingClientRect().bottom;
     var windowBottom = window.innerHeight;
 
     if (endSectionBottom <= windowBottom) {
-        gifContainer.style.opacity = 1;
-        audio.play();
+      gifContainer.style.opacity = 1;
+      audio.play();
     } else {
-        gifContainer.style.opacity = 0;
-        audio.pause();
-        audio.currentTime = 0;
+      gifContainer.style.opacity = 0;
+      audio.pause();
+      audio.currentTime = 0;
     }
-});
-function openImage() {
+  });
+
+  function openImage() {
     var modal = document.getElementById("myModal");
     var img = document.getElementById("myImage");
     var modalImg = document.getElementById("modalImg");
 
     modal.style.display = "block";
     modalImg.src = img.src;
-}
+  }
 
-function closeImage() {
+  function closeImage() {
     document.getElementById("myModal").style.display = "none";
-}
+  }
 
-window.onclick = function(event) {
+  window.onclick = function(event) {
     var modal = document.getElementById("myModal");
     if (event.target === modal) {
-        modal.style.display = "none";
+      modal.style.display = "none";
     }
-};
+  };
+});
